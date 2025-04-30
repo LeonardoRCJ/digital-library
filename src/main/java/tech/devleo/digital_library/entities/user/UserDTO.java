0880e6c403793a3a9c66775d3cfd63d53e16c9ef
Leonardo Rocha <leonardorocha790@gmail.com>
2025-04-30 09:16:54 -0300

    feat: add toEntity method in UserDTO

--- src/main/java/tech/devleo/digital_library/entities/user/UserDTO.java
+++ src/main/java/tech/devleo/digital_library/entities/user/UserDTO.java
@@ -1,4 +1,7 @@
 package tech.devleo.digital_library.entities.user;
 
 public record UserDTO(String fullName, String cpf, String email, String password, UserType userType) {
+    public User  toEntity(){
+        return new User(null, fullName, cpf, email, password, userType);
+    }
 }
