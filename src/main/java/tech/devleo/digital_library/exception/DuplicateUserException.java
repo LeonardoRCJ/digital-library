d0abd497dc163eec663d4668d816ec972b1bb18f
Leonardo Rocha <leonardorocha790@gmail.com>
2025-04-30 09:13:44 -0300

    fix: rename user duplicate exception

---
+++ src/main/java/tech/devleo/digital_library/exception/DuplicateUserException.java
@@ -0,0 +1,7 @@
+package tech.devleo.digital_library.exception;
+
+public class DuplicateUserException extends RuntimeException {
+    public DuplicateUserException(String message) {
+        super(message);
+    }
+}
