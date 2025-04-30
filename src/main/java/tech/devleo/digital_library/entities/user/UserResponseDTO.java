264b3e96f1f4dadd9ec4e3dee91c7067db32973d
Leonardo Rocha <leonardorocha790@gmail.com>
2025-04-30 09:19:03 -0300

    feat: add user response

--- src/main/java/tech/devleo/digital_library/entities/user/UserResponseDTO.java
+++ src/main/java/tech/devleo/digital_library/entities/user/UserResponseDTO.java
@@ -1,4 +1,7 @@
 package tech.devleo.digital_library.entities.user;
 
 public record UserResponseDTO(String fullName, String email, String cpf) {
+    public UserResponseDTO(User user) {
+        this(user.getFullName(), user.getEmail(), user.getCpf());
+    }
 }
